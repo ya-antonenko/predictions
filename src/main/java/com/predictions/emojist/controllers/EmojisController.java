fc7f62c99cba8f5d9664ff744347683b72ae39b9
ya-antonenko <antonenkojaroslav@gmail.com>
2020-06-28 15:37:02 +0300

    refactoring

--- src/main/java/com/predictions/emojist/controllers/EmojisController.java
+++ src/main/java/com/predictions/emojist/controllers/EmojisController.java
@@ -1,13 +1,14 @@
 package com.predictions.emojist.controllers;
 
 import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmojisController {
-    @PostMapping("/predictions")
+    @GetMapping("/predictions")
     public JSONObject predictionsToJson(@RequestParam("emojis") String emojis){
         JSONObject jsonObject = new JSONObject();
         jsonObject.put(emojis, Math.random());
